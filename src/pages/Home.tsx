72d1ed40b2e30fdc82ea1d5ca45b0661a8262fef
uriapete <uriarteangelo@gmail.com>
2023-07-10 12:39:34 -0700

    pages/Home - add test text and styling

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -4,7 +4,7 @@ import "./styles/Home.scss"
 export default function Home():ReactElement{
     return(
         <div className="comp comp-home">
-            
+            <h1>test</h1>
         </div>
     )
 }
