96f1d11e121ae40b29466a43bf0c9c89009eb301
uriapete <uriarteangelo@gmail.com>
2023-07-10 16:06:11 -0700

    App - fix game route not set properly

--- src/App.tsx
+++ src/App.tsx
@@ -9,7 +9,7 @@ function App() {
       
       <Routes>
         <Route path='' element={Home()} />
-        <Route path='/game' element={Game()} />
+        <Route path='game' element={Game()} />
       </Routes>
     </div>
   );
