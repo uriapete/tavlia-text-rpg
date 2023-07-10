0aa115e3a1e22e5b2ad0a695349e367d5293ccf0
uriapete <uriarteangelo@gmail.com>
2023-07-10 15:57:44 -0700

    pages/Home - create basic home page

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,10 +1,18 @@
 import { ReactElement } from "react";
+import { Link } from "react-router-dom";
 import "./styles/Home.scss"
 
 export default function Home():ReactElement{
     return(
         <div className="comp comp-home">
-            <h1>test</h1>
+            <h1 className="home-title">Tavlia</h1>
+            <p>Hello! Welcome to Tavlia!</p>
+            <p>Currently a work in progress!</p>
+            <p>
+                Wanna play?
+            </p>
+            <Link to={"#"} className="home-play-btn" id="home-play-btn">Play now!</Link>
+
         </div>
     )
 }
