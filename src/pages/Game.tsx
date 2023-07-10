db185a290a55254814d68925162ae60501e75b6c
uriapete <uriarteangelo@gmail.com>
2023-07-10 16:02:51 -0700

    pages - init Game page

---
+++ src/pages/Game.tsx
@@ -0,0 +1,8 @@
+import { ReactElement } from "react";
+import './styles/Game.scss'
+
+export default function Game():ReactElement{
+    return(
+        <div className="page page-game"></div>
+    )
+}
