afe994fdf976c720049f3d4ae115771e81b8bea2
uriapete <uriarteangelo@gmail.com>
2023-07-10 16:04:01 -0700

    App - add new '/game' route with Game element

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import './App.css';
 import { Route,Routes } from "react-router";
 import Home from './pages/Home';
+import Game from './pages/Game';
 
 function App() {
   return (
@@ -8,6 +9,7 @@ function App() {
       
       <Routes>
         <Route path='' element={Home()} />
+        <Route path='/game' element={Game()} />
       </Routes>
     </div>
   );
