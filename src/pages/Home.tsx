058f24be64120ab779d35892402a4f7c3ab54e56
uriapete <uriarteangelo@gmail.com>
2023-07-10 12:35:59 -0700

    pages - init Home component and styling

---
+++ src/pages/Home.tsx
@@ -0,0 +1,10 @@
+import { ReactElement } from "react";
+import "./styles/Home.scss"
+
+export default function Home():ReactElement{
+    return(
+        <div className="comp comp-home">
+            
+        </div>
+    )
+}
