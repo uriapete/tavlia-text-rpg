c98154d7be387bd941de9feef53b555c9a44f32d
uriapete <uriarteangelo@gmail.com>
2023-07-11 15:55:40 -0700

    gameData/interfaces/IItem - add nums to IItem

--- src/gameData/interfaces/IItem.ts
+++ src/gameData/interfaces/IItem.ts
@@ -3,5 +3,7 @@ import IEntity from "./entities/IEntity";
 export default interface IItem{
     name:string;
     bio:string;
+    primaryNum?:number;
+    secondaryNums?:number[];
     use?(target?:IEntity):number|string|null;
 }
