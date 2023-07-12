9d04646044cb4e76d679f58bb0ead206a9273649
uriapete <uriarteangelo@gmail.com>
2023-07-11 19:21:55 -0700

    gamedata/classes/skills/BeneficialSkill - implement skill effect

--- src/gameData/classes/skills/BeneficialSkill.ts
+++ src/gameData/classes/skills/BeneficialSkill.ts
@@ -1,3 +1,4 @@
+import IEntity from "../../interfaces/entities/IEntity";
 import IBeneficialSkill from "../../interfaces/skills/IBeneficialSkill";
 import ISkill from "../../interfaces/skills/ISkill";
 import Skill from "./Skill";
@@ -6,11 +7,20 @@ export default class BeneficialSkill extends Skill implements IBeneficialSkill,I
     constructor(
         name:string,bio:string="",
         protected _manaCost:number=0,
+        private healPower:number
     ){
         super(name,bio)
     }
 
     get manaCost(){
         return this._manaCost
+    }
+
+    public effect(user:IEntity,target=user):boolean{
+        if(user.useMana(this.manaCost)===false){
+            return false
+        }
+        target.healHP(this.healPower)
+        return true
     }
 }
