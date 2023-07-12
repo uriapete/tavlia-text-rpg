f4383744ae31c6cde097d632aae5ad31cef0fe2a
uriapete <uriarteangelo@gmail.com>
2023-07-11 18:27:02 -0700

    gamedata/classes/skills/PhysicalAttack - implement attack method, proper constructor

--- src/gameData/classes/skills/PhysicalAttack.ts
+++ src/gameData/classes/skills/PhysicalAttack.ts
@@ -1,7 +1,16 @@
+import IEntity from "../../interfaces/entities/IEntity";
 import IPhysicalAttack from "../../interfaces/skills/IPhysicalAttack";
 import ISkill from "../../interfaces/skills/ISkill";
 import Skill from "./Skill";
 
 export default class PhysicalAttack extends Skill implements IPhysicalAttack,ISkill{
-    
+    constructor(
+        name:string,bio="",
+        private power:number
+    ){
+        super(name,bio)
+    }
+    public attack(user:IEntity,target:IEntity){
+        target.takePhysDmg(user.physAtt*this.power)
+    }
 }
