c0c3004b84c32a0c4093040bfbfd5d32b5412ed1
uriapete <uriarteangelo@gmail.com>
2023-07-12 15:50:01 -0700

    gamedata/classes/locations/DungeonLevel - move initiation of cleared property to constructor

--- src/gameData/classes/locations/DungeonLevel.ts
+++ src/gameData/classes/locations/DungeonLevel.ts
@@ -4,13 +4,14 @@ import IGameLocation from "../../interfaces/locations/IGameLocation";
 import Enemy from "../entities/Enemy";
 
 export default class DungeonLevel extends GameLocation implements IDungeonLevel, IGameLocation {
-    public cleared = false
+    public cleared:boolean
     constructor(
         private _enemies:Enemy[],
         bio="",
         name="",
     ){
         super(name,bio)
+        this.cleared=false
     }
 
     public get enemies(){
