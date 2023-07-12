19469bad148e7ed405d65c090497238f3494b415
uriapete <uriarteangelo@gmail.com>
2023-07-12 15:33:09 -0700

    gamedata/interfaces/ICampaign - add player char prop

--- src/gameData/interfaces/ICampaign.ts
+++ src/gameData/interfaces/ICampaign.ts
@@ -1,3 +1,4 @@
+import PlayerChar from "../classes/entities/PlayerChar";
 import IGameLocation from "./locations/IGameLocation";
 
 export interface IGameLocationConnections{
@@ -6,6 +7,7 @@ export interface IGameLocationConnections{
 }
 
 export default interface ICampaign{
+    playerChar:PlayerChar
     locations:IGameLocationConnections[]
     flag:number
     completionFlag:number
