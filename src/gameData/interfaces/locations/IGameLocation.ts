71ba771250fa5c3a87208fc2c576e840cc804adc
uriapete <uriarteangelo@gmail.com>
2023-07-15 08:58:28 -0700

    gameData/interfaces/locations/IGameLocation.ts - add visited flag, add access flag enum, interface, and prop

--- src/gameData/interfaces/locations/IGameLocation.ts
+++ src/gameData/interfaces/locations/IGameLocation.ts
@@ -1,4 +1,16 @@
+export enum AccessFlagTypes {
+    visited = "visited",
+    cleared = "cleared",
+}
+
+export interface IAccessFlag {
+    type: AccessFlagTypes,
+    location: IGameLocation,
+}
+
 export default interface IGameLocation{
     name?:string
     bio?:string
+    accessFlag?:IAccessFlag
+    visited?:boolean
 }
