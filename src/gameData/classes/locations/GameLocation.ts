ab49c111407d67fbb94a4059634746e2a3452b52
uriapete <uriarteangelo@gmail.com>
2023-07-15 08:59:21 -0700

    gameData/classes/locations/GameLocation.ts - implement access flag interface into class, implement visited flag prop and accessflag prop into GL class

--- src/gameData/classes/locations/GameLocation.ts
+++ src/gameData/classes/locations/GameLocation.ts
@@ -1,10 +1,27 @@
-import IGameLocation from "../../interfaces/locations/IGameLocation";
+import IGameLocation, { AccessFlagTypes, IAccessFlag } from "../../interfaces/locations/IGameLocation";
+
+export class AccessFlag implements IAccessFlag{
+    constructor(
+        private _type:AccessFlagTypes,
+        private _location:GameLocation
+    ){}
+    public get type(){
+        return this._type
+    }
+    public get location(){
+        return this._location
+    }
+}
 
 export default abstract class GameLocation implements IGameLocation{
+    private _visited:boolean
     constructor(
         private _name:string,
         private _bio:string="",
-    ){}
+        private _accessFlag?:AccessFlag
+    ){
+        this._visited=false
+    }
 
     get name(){
         return this._name
@@ -12,5 +29,13 @@ export default abstract class GameLocation implements IGameLocation{
 
     get bio(){
         return this._bio
+    }
+    
+    get visited(){
+        return this._visited
+    }
+
+    get accessFlag(){
+        return this._accessFlag
     }
 }
