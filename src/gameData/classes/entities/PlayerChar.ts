e9e6d3025a8e310955fadc6824f5bcbb4848cbcc
uriapete <uriarteangelo@gmail.com>
2023-07-15 20:31:08 -0700

    gameData/classes/entities/PlayerChar.ts - add 'isPlayerChar' prop to PlayerChar class to silence 'useless constructor' error

--- src/gameData/classes/entities/PlayerChar.ts
+++ src/gameData/classes/entities/PlayerChar.ts
@@ -3,10 +3,15 @@ import Skill from "../skills/Skill";
 import Entity from "./Entity";
 
 export default class PlayerChar extends Entity implements IPlayerChar, Entity {
+    private _isPlayerChar:boolean
     constructor(
         maxHP: number, maxMana: number, physAtt: number, physDef: number, magicAtt: number, magicDef: number, speed: number, basicSkill: Skill,
     ) {
         super(maxHP, maxMana, physAtt, physDef, magicAtt, magicDef, speed, basicSkill,)
+        this._isPlayerChar=true
+    }
+    get isPlayerChar(){
+        return this._isPlayerChar
     }
 }
 
