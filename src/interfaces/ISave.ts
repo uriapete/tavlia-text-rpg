11b3bd71ad1e1f7d0f737b51dfe3f58c6178e068
uriapete <uriarteangelo@gmail.com>
2023-07-17 15:49:55 -0700

    interfaces/ISave.ts - add text to save

--- src/interfaces/ISave.ts
+++ src/interfaces/ISave.ts
@@ -1,6 +1,8 @@
+import { ReactElement } from "react";
 import Campaign, { GameLocationConnections } from "../gameData/classes/Campaign";
 
 export default interface ISave{
     campaign:Campaign,
     location:GameLocationConnections,
+    text?:ReactElement[]
 }
