c4fe67901a024f84614c69ab8bb1208391294312
uriapete <uriarteangelo@gmail.com>
2023-07-18 00:47:33 -0700

    functions/getUserFromToken.ts - authorize the right way

--- src/functions/getUserFromToken.ts
+++ src/functions/getUserFromToken.ts
@@ -5,10 +5,13 @@ export default async function getUserFromToken(token:string){
     const response = await fetch(
         `${process.env.REACT_APP_API_URL}/user_from_token/`,
         {
-            body: JSON.stringify({ token }), method: "POST", headers: {
+            method: "GET", headers: {
                 'Accept': 'application/json',
-                'Content-Type': 'application/json'
-            }, }
+                'Content-Type': 'application/json',
+                'Authorization': `Token ${token}`,
+            },
+            
+        }
     )
     const respData: UserData = await response.json()
     return respData
