a5091bedf07b43b179bbc2cc08dcdea1d71207b9
uriapete <uriarteangelo@gmail.com>
2023-07-18 11:44:22 -0700

    hooks/useLoadSave.ts - add locations param

--- src/hooks/useLoadSave.ts
+++ src/hooks/useLoadSave.ts
@@ -2,8 +2,9 @@ import { useContext, useEffect, useState } from "react"
 import { UserToken } from "./Contexts"
 import SaveResponse from "../interfaces/SaveResponse"
 import ISaveData from "../interfaces/ISaveData"
+import { GameLocationConnections } from "../gameData/classes/Campaign"
 
-export default function useLoadSave(saveID?:number|null){
+export default function useLoadSave(locations:GameLocationConnections[],saveID?:number|null){
     const TokenContext = useContext(UserToken)
     const [saveData, setSaveData] = useState<null|SaveResponse>(null)
     useEffect(()=>{
