455e0c7cf812440d21cdba4e5bd215eecefc73f2
uriapete <uriarteangelo@gmail.com>
2023-07-18 12:34:25 -0700

    hooks/useLoadSave.ts - create save load function

--- src/hooks/useLoadSave.ts
+++ src/hooks/useLoadSave.ts
@@ -1,15 +1,68 @@
-import { useContext, useEffect, useState } from "react"
+import { useContext, useEffect } from "react"
 import { UserToken } from "./Contexts"
 import SaveResponse from "../interfaces/SaveResponse"
-import ISaveData from "../interfaces/ISaveData"
 import { GameLocationConnections } from "../gameData/classes/Campaign"
+import { AccessFlagTypes } from "../gameData/interfaces/ICampaign"
 
-export default function useLoadSave(locations:GameLocationConnections[],saveID?:number|null){
+export default function useLoadSave(
+    locations: GameLocationConnections[],
+    setCurrLoc: (gameLoc:GameLocationConnections)=>any,
+    saveID?:number|null
+){
     const TokenContext = useContext(UserToken)
-    const [saveData, setSaveData] = useState<null|SaveResponse>(null)
+    
     useEffect(()=>{
-        function getSaveData(){}
-    },[])
-    return saveData
+        async function getSaveData(){
+            if(TokenContext===null||typeof saveID==="undefined"||saveID===null){
+                return null
+            }
+            const{userToken}=TokenContext
 
+            const response = await fetch(
+                `${process.env.REACT_APP_API_URL}/save_files/${saveID}`,
+                {
+                    method: "GET",
+                    headers: {
+                        'Accept': 'application/json',
+                        'Content-Type': 'application/json',
+                        'Authorization': `Token ${userToken}`,
+                    },
+                }
+            )
+
+            const respData: SaveResponse = await response.json()
+
+            let flagCount=0
+
+            for (let i = 1; i < locations.length; i++) {
+                const location = locations[i];
+
+                if(i===respData.current_location){
+                    setCurrLoc(location)
+                }
+
+                if(flagCount<respData.flags){
+                    if(typeof location.accessFlag!=="undefined"){
+                        switch (location.accessFlag.type) {
+                            case AccessFlagTypes.Visited:
+                                location.accessFlag.location.visit()
+                                break;
+                        
+                            default:
+                                break;
+                        }
+                    }
+                }
+
+                if(i>=respData.current_location&&flagCount>=respData.flags){
+                    break
+                }
+            }
+        }
+        try {
+            getSaveData()
+        } catch (error) {
+            console.log(error)
+        }
+    },[TokenContext,locations,saveID,setCurrLoc])
 }
