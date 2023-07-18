09c184ffd183a46e7ab3ee60ba12bd59a3c92429
uriapete <uriarteangelo@gmail.com>
2023-07-18 03:19:43 -0700

    functions/saveGame.ts - create save data

--- src/functions/saveGame.ts
+++ src/functions/saveGame.ts
@@ -1,11 +1,43 @@
 import Campaign, { GameLocationConnections } from "../gameData/classes/Campaign";
+import { AccessFlagTypes } from "../gameData/interfaces/ICampaign";
 import ISaveData from "../interfaces/ISaveData";
 
 export default async function saveGame(userToken:string, campaign:Campaign,location?:GameLocationConnections){
+    let flags=0
+    let loc=0
+    let checkForCurrLoc=true
+    let checkForFlags=true
+    for (let i = 0; i < campaign.locations.length; i++) {
+        const gameLocCon = campaign.locations[i];
+        const accFlag=gameLocCon.accessFlag
+
+        if(checkForCurrLoc){
+            if(gameLocCon===location){
+                loc=i
+                checkForCurrLoc=false
+            }
+        }
+
+        if(checkForFlags){
+            if(typeof accFlag==="undefined"){
+                continue
+            }
+            if(accFlag.type===AccessFlagTypes.Visited){
+                if(!accFlag.location.visited){
+                    checkForFlags=false
+                }else{
+                    flags++
+                }
+            }
+        }
+        if(!checkForCurrLoc&&!checkForFlags){
+            break
+        }
+    }
 
     const saveData:ISaveData={
-        campaign_data:JSON.stringify(campaign),
-        current_location:JSON.stringify(location),
+        flags,
+        current_location:loc,
     }
 
     const response = await fetch(
